5f4ee525c2d48717f6be1f24a01a1f4eaff200df
cccanvas-upv <cccanvas@doctor.upv.es>
2026-02-12 12:21:41 +0100

    Updated main and utils

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from utils import plot_trace
-from lab_devices import Yeni
+from utils import plot_trace, save_trace
+from lab_devices import Yeni, YAMLDOCUMENT
+
+import os, datetime, time
+from os.path import isfile, expanduser
+
+timestamp = datetime.datetime.fromtimestamp(time.time())
+formatted = timestamp.strftime("%Y_%m_%d_%H_%M_%S")
+UNDEFINED = "UNDEFINED"
+
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..\..')))
 
 def main():
     print("Hello from test-osa!")
@@ -12,7 +22,30 @@ def main():
     except Exception as e:
         raise ConnectionError(f"Could not connect to OSA:\n {e}")
 
+    # Configure the YAML document
+    doc = YAMLDOCUMENT()
+    doc.datetime = formatted
+    doc.operator = 'Camilo Cano'
+    doc.setup = 'P2.S1'
+    doc.project = project = UNDEFINED
+    doc.wafer = wafer = UNDEFINED
+    doc.reticle = reticle = UNDEFINED
+    doc.die = die_name = UNDEFINED
+    doc.dut = dut = "wvg"
+    doc.polarization = polarization = 'nana'
+    doc.die_temperature = "na" #kOhm Tacc
+    doc.coupling_type = 'SM-SM'
+    doc.idsource = "ASE1"#'FiberLabs ASE-FL7015 1530-1610nm'
+    doc.idosa = 'OSA20' # "EXFO OSA20"
+    doc.operator_notes = """NA"""
 
+    # Saving data
+    home = expanduser("~")
+    folder_lab = os.path.expandvars("%USERPROFILE%\\data")
+    folder_measurements = "\\".join([folder_lab,project,wafer,reticle,die_name])
+    file_name = f"{dut}-{polarization}"
+    file_path = "\\".join([folder_measurements,file_name])
+    
     # Work with the OSA
     with osa: 
         osa_idn = osa.id
@@ -30,9 +63,23 @@ def main():
         )
         osa.run_sweep(tracename, averages=averages)
         trace = osa.get_trace(tracename)
-
-        # Plot the trace
-        plot_trace(trace)
+        doc.coupler_2X2_90_10_SN = "none"
+ 
+        ## Editing the yaml document
+        doc.osa_idn = osa_idn
+        doc.osa_resolution = osa.resolution_bandwidth
+        doc.osa_sensitivity = osa.sensitivity_level
+        doc.osa_start_wavelength = osa.wavelength_center-osa.wavelength_span/2 # do not use osa.start_wavelength it gives lot of digits
+        doc.osa_stop_wavelength =  osa.wavelength_center+osa.wavelength_span/2 # do not use osa.stop_wavelength it gives lot of digits
+        doc.osa_sweep_mode = osa.sweep_mode
+        doc.osa_integrated_power = osa.integrated_power
+        doc.osa_dBm_nm = osa.dBm_nm
+                # Saving txt from osa measurement
+        save_trace(folder_measurements=folder_measurements,
+                file_name=file_name,
+                yaml=doc,osa=osa,data=trace)
+    # Plot the trace
+    plot_trace(trace)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -2,6 +2,34 @@ import numpy as np
 import matplotlib
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
+import os
+from os.path import isfile, expanduser
+
+def save_trace(folder_measurements,file_name, yaml, osa, data):
+    yaml.osa_idn = osa.id
+    yaml.osa_resolution = osa.resolution_bandwidth
+    yaml.osa_sensitivity = osa.sensitivity_level
+    yaml.osa_start_wavelength = osa.wavelength_center-osa.wavelength_span/2
+    yaml.osa_stop_wavelength =  osa.wavelength_center+osa.wavelength_span/2
+    yaml.osa_sweep_mode = osa.sweep_mode
+    yaml.osa_integrated_power = osa.integrated_power
+    yaml.osa_dBm_nm = osa.dBm_nm
+    os.makedirs(folder_measurements, exist_ok=True)
+    filename = '\\'.join([folder_measurements,file_name])
+    header = '\n'.join([' ' + str(a) + ': ' + str(b) for a, b in yaml.__dict__.items()])
+    if osa.dBm_nm:
+        header = header + '\n\n' + ' Wavelength [nm] \t Power [dBm/nm]'
+    else:
+        header = header + '\n\n' + ' Wavelength [nm] \t Power [dBm]'
+    if isfile(filename + '.txt'):
+        i = 0
+        filename = filename + '_copy' + str(i)
+        while isfile(filename+'.txt'):
+            i = i+1
+            filename = filename[:-1] + str(i)
+    np.savetxt(filename + '.txt', [list(i) for i in zip(*data)], fmt='%2.10f', delimiter='\t', header=header,
+                comments='%')
+
 
 def dBmtoW(P_dBm):
     return 10**(P_dBm/10)*1E-3  
